125315ce540b30b89ff71f1922e353372d274cb0
Matthieu Locussol <matthieu@galadrim.fr>
2021-02-25 19:48:37 +0100

    feat: removed cmd file & updated main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,28 +3,6 @@
   windows_subsystem = "windows"
 )]
 
-mod cmd;
-
 fn main() {
-  tauri::AppBuilder::new()
-    .invoke_handler(|_webview, arg| {
-      use cmd::Cmd::*;
-      match serde_json::from_str(arg) {
-        Err(e) => {
-          Err(e.to_string())
-        }
-        Ok(command) => {
-          match command {
-            // definitions for your custom commands from Cmd here
-            MyCustomCommand { argument } => {
-              //  your command code
-              println!("{}", argument);
-            }
-          }
-          Ok(())
-        }
-      }
-    })
-    .build()
-    .run();
+  tauri::AppBuilder::new().build().run();
 }
